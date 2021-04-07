e8429cda22c8e817846ecdf728f6f36bcb465947
andresma2490 <andresma2490@gmail.com>
2021-04-07 14:06:08 -0500

    adding bootstrap and staticfiles conf

--- myBlog/settings.py
+++ myBlog/settings.py
@@ -129,4 +129,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATICFILES_DIRS = (
+    BASE_DIR/'staticfiles',
+)
+
 STATIC_URL = '/static/'

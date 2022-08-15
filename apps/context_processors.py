d150e75b4a679937ac993d3a595856e7df13378a
andresma2490 <montenegro.andres@correounivalle.edu.co>
2022-08-15 18:12:40 -0500

    improving layout

--- apps/context_processors.py
+++ apps/context_processors.py
@@ -1,4 +1,9 @@
 from apps.articles.models import Category
+from django.conf import settings
+
+
+def debug(request):
+  return {'DEBUG': settings.DEBUG}
 
 def getCategories(request):
     return {'categories': Category.objects.all()}
--- myBlog/settings/base.py
+++ myBlog/settings/base.py
@@ -74,7 +74,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'apps.context_processors.getCategories'
+                'apps.context_processors.debug',
+                'apps.context_processors.getCategories',
             ],
         },
     },
